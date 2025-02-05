615d2dfbae1d4d762d4545a2b0cd6c0a063a6cfe
Matt Perry <mattgperry@gmail.com>
2025-02-05 11:04:39 +0100

    Adding test

--- packages/framer-motion/cypress/integration/animate-presence-pop.ts
+++ packages/framer-motion/cypress/integration/animate-presence-pop.ts
@@ -13,193 +13,202 @@ function expectBbox(element: HTMLElement, expectedBbox: Partial<BoundingBox>) {
     expectedBbox.height && expect(bbox.height).to.equal(expectedBbox.height)
 }
 
-describe("AnimatePresence popLayout", () => {
-    it("correctly pops exiting elements out of the DOM", () => {
-        cy.visit("?test=animate-presence-pop")
-            .wait(50)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 200,
-                    left: 100,
-                    width: 100,
-                    height: 100,
-                })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 300,
-                    left: 100,
-                    width: 100,
-                    height: 100,
-                })
-            })
-            .trigger("click", 60, 60, { force: true })
-            .wait(100)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 200,
-                    left: 100,
-                    width: 100,
-                    height: 100,
-                })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 200,
-                    left: 100,
-                    width: 100,
-                    height: 100,
-                })
-            })
-            .trigger("click", 60, 60, { force: true })
-            .wait(100)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 200,
-                    left: 100,
-                    width: 100,
-                    height: 100,
-                })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 300,
-                    left: 100,
-                    width: 100,
-                    height: 100,
-                })
-            })
-    })
+function runTests(url: string) {
+    const name =
+        "AnimatePresence popLayout" +
+        (url.includes("layout-container") ? " with layout animations" : "")
 
-    it("correctly pops exiting elements out of the DOM when they already have an explicit top/left", () => {
-        cy.visit("?test=animate-presence-pop&position=relative")
-            .wait(50)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 300,
-                    left: 200,
-                    width: 100,
-                    height: 100,
+    describe(name, () => {
+        it("correctly pops exiting elements out of the DOM", () => {
+            cy.visit(url)
+                .wait(50)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 200,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 400,
-                    left: 200,
-                    width: 100,
-                    height: 100,
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 300,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .trigger("click", 60, 60, { force: true })
-            .wait(100)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 300,
-                    left: 200,
-                    width: 100,
-                    height: 100,
+                .trigger("click", 60, 60, { force: true })
+                .wait(100)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 200,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 300,
-                    left: 200,
-                    width: 100,
-                    height: 100,
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 200,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .trigger("click", 60, 60, { force: true })
-            .wait(100)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 300,
-                    left: 200,
-                    width: 100,
-                    height: 100,
+                .trigger("click", 60, 60, { force: true })
+                .wait(100)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 200,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 400,
-                    left: 200,
-                    width: 100,
-                    height: 100,
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 300,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-    })
+        })
 
-    it("correctly pops exiting elements out of the DOM when anchorX is set to right", () => {
-        cy.visit("?test=animate-presence-pop&anchor-x=right")
-            .wait(50)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 200,
-                    left: 100,
-                    width: 100,
-                    height: 100,
+        it("correctly pops exiting elements out of the DOM when they already have an explicit top/left", () => {
+            cy.visit(url + "&position=relative")
+                .wait(50)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 300,
+                        left: 200,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 300,
-                    left: 100,
-                    width: 100,
-                    height: 100,
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 400,
+                        left: 200,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .trigger("click", 60, 60, { force: true })
-            .wait(100)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 200,
-                    left: 100,
-                    width: 100,
-                    height: 100,
+                .trigger("click", 60, 60, { force: true })
+                .wait(100)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 300,
+                        left: 200,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 200,
-                    left: 100,
-                    width: 100,
-                    height: 100,
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 300,
+                        left: 200,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .trigger("click", 60, 60, { force: true })
-            .wait(100)
-            .get("#b")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 200,
-                    left: 100,
-                    width: 100,
-                    height: 100,
+                .trigger("click", 60, 60, { force: true })
+                .wait(100)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 300,
+                        left: 200,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
-            .get("#c")
-            .should(([$a]: any) => {
-                expectBbox($a, {
-                    top: 300,
-                    left: 100,
-                    width: 100,
-                    height: 100,
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 400,
+                        left: 200,
+                        width: 100,
+                        height: 100,
+                    })
                 })
-            })
+        })
+
+        it("correctly pops exiting elements out of the DOM when anchorX is set to right", () => {
+            cy.visit(url + "&anchor-x=right")
+                .wait(50)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 200,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
+                })
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 300,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
+                })
+                .trigger("click", 60, 60, { force: true })
+                .wait(100)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 200,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
+                })
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 200,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
+                })
+                .trigger("click", 60, 60, { force: true })
+                .wait(100)
+                .get("#b")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 200,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
+                })
+                .get("#c")
+                .should(([$a]: any) => {
+                    expectBbox($a, {
+                        top: 300,
+                        left: 100,
+                        width: 100,
+                        height: 100,
+                    })
+                })
+        })
     })
-})
+}
+
+runTests("?test=animate-presence-pop")
+runTests("?test=animate-presence-pop-layout-container")
