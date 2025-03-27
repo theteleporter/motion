c52146199e5ce45788dec99378aa7cb470bd63cb
Matt Perry <mattgperry@gmail.com>
2025-03-27 12:43:13 +0100

    Adding willChange injection

--- packages/motion-utils/src/__tests__/velocity-per-second.test.ts
+++ packages/motion-utils/src/__tests__/velocity-per-second.test.ts
@@ -1,4 +1,4 @@
-import { velocityPerSecond } from "motion-utils"
+import { velocityPerSecond } from "../velocity-per-second"
 
 test("velocityPerSecond", () => {
     expect(velocityPerSecond(0.835, 16.7)).toBe(50)
