febe62e354d49463e85fec941c3c4253b3679c38
Matt Perry <mattgperry@gmail.com>
2025-04-02 13:49:35 +0200

    Latest

---
+++ dev/react/src/tests/scroll-delay.tsx
@@ -0,0 +1,38 @@
+import { scroll } from "motion"
+import { useAnimate } from "motion/react"
+import { useEffect } from "react"
+
+export const App = () => {
+    const [scope, animate] = useAnimate()
+
+    useEffect(() => {
+        scroll(animate("#without-delay", { x: "500px" }, { duration: 1 }))
+        scroll(
+            animate(
+                "#with-delay",
+                { transform: "translateX(500px)" },
+                { delay: 0.5, duration: 1 }
+            )
+        )
+    }, [])
+    return (
+        <div
+            ref={scope}
+            style={{
+                height: "200vh",
+                background: "linear-gradient(#fff, #000)",
+            }}
+        >
+            <div style={{ position: "fixed", top: 0, left: 0 }}>
+                <div
+                    id="without-delay"
+                    style={{ width: 100, height: 100, background: "red" }}
+                />
+                <div
+                    id="with-delay"
+                    style={{ width: 100, height: 100, background: "blue" }}
+                />
+            </div>
+        </div>
+    )
+}
