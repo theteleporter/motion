7577c87e3729482b5146ea5090d483cc0bdda69c
Matt Perry <mattgperry@gmail.com>
2025-04-16 15:56:36 +0200

    Speedup for animate Mini

--- packages/framer-motion/src/animation/animators/waapi/animate-elements.ts
+++ packages/framer-motion/src/animation/animators/waapi/animate-elements.ts
@@ -11,9 +11,21 @@ import {
     getComputedStyle,
     getValueTransition,
     NativeAnimation,
+    NativeAnimationOptions,
     resolveElements,
+    UnresolvedValueKeyframe,
+    ValueKeyframe,
 } from "motion-dom"
 import { invariant, secondsToMilliseconds } from "motion-utils"
+
+interface AnimationDefinition {
+    map: Map<string, NativeAnimation<any>>
+    key: string
+    unresolvedKeyframes: UnresolvedValueKeyframe[]
+    options: Omit<NativeAnimationOptions, "keyframes"> & {
+        keyframes?: ValueKeyframe[]
+    }
+}
 
 export function animateElements(
     elementOrSelector: ElementOrSelector,
@@ -28,8 +40,28 @@ export function animateElements(
 
     invariant(Boolean(numElements), "No valid element provided.")
 
-    const animations: AnimationPlaybackControls[] = []
+    /**
+     * WAAPI doesn't support interrupting animations.
+     *
+     * Therefore, starting animations requires a three-step process:
+     * 1. Stop existing animations (write styles to DOM)
+     * 2. Resolve keyframes (read styles from DOM)
+     * 3. Create new animations (write styles to DOM)
+     *
+     * The hybrid `animate()` function uses AsyncAnimation to resolve
+     * keyframes before creating new animations, which removes style
+     * thrashing. Here, we have much stricter filesize constraints.
+     * Therefore we do this in a synchronous way that ensures that
+     * at least within `animate()` calls there is no style thrashing.
+     *
+     * In the motion-native-animate-mini-interrupt benchmark this
+     * was 80% faster than a single loop.
+     */
+    const animationDefinitions: AnimationDefinition[] = []
 
+    /**
+     * Step 1: Build options and stop existing animations (write)
+     */
     for (let i = 0; i < numElements; i++) {
         const element = elements[i]
         const elementTransition = { ...options }
@@ -55,52 +87,78 @@ export function animateElements(
             valueOptions.duration &&= secondsToMilliseconds(
                 valueOptions.duration
             )
+
             valueOptions.delay &&= secondsToMilliseconds(valueOptions.delay)
 
             /**
              * If there's an existing animation playing on this element then stop it
              * before creating a new one.
              */
-            const animationMap = getAnimationMap(element)
-            const stateKey = animationMapKey(
+            const map = getAnimationMap(element)
+            const key = animationMapKey(
                 valueName,
                 valueOptions.pseudoElement || ""
             )
-            const currentAnimation = animationMap.get(stateKey)
+            const currentAnimation = map.get(key)
             currentAnimation && currentAnimation.stop()
 
-            const { pseudoElement } = valueOptions
-            if (!pseudoElement && valueKeyframes[0] === null) {
-                valueKeyframes[0] = getComputedStyle(element, valueName)
-            }
-
-            fillWildcards(valueKeyframes)
-            applyPxDefaults(valueKeyframes, valueName)
-
-            /**
-             * If we only have one keyframe, explicitly read the initial keyframe
-             * from the computed style. This is to ensure consistency with WAAPI behaviour
-             * for restarting animations, for instance .play() after finish, when it
-             * has one vs two keyframes.
-             */
-            if (!pseudoElement && valueKeyframes.length < 2) {
-                valueKeyframes.unshift(getComputedStyle(element, valueName))
-            }
-
-            const animation = new NativeAnimation({
-                ...valueOptions,
-                element,
-                name: valueName,
-                keyframes: valueKeyframes,
-                allowFlatten:
-                    !elementTransition.type && !elementTransition.ease,
+            animationDefinitions.push({
+                map,
+                key,
+                unresolvedKeyframes: valueKeyframes,
+                options: {
+                    ...valueOptions,
+                    element,
+                    name: valueName,
+                    allowFlatten:
+                        !elementTransition.type && !elementTransition.ease,
+                },
             })
-
-            animation.finished.finally(() => animationMap.delete(stateKey))
-
-            animations.push(animation)
-            animationMap.set(stateKey, animation)
         }
+    }
+
+    /**
+     * Step 2: Resolve keyframes (read)
+     */
+    for (let i = 0; i < animationDefinitions.length; i++) {
+        const { unresolvedKeyframes, options: animationOptions } =
+            animationDefinitions[i]
+
+        const { element, name, pseudoElement } = animationOptions
+        if (!pseudoElement && unresolvedKeyframes[0] === null) {
+            unresolvedKeyframes[0] = getComputedStyle(element, name)
+        }
+
+        fillWildcards(unresolvedKeyframes)
+        applyPxDefaults(unresolvedKeyframes, name)
+
+        /**
+         * If we only have one keyframe, explicitly read the initial keyframe
+         * from the computed style. This is to ensure consistency with WAAPI behaviour
+         * for restarting animations, for instance .play() after finish, when it
+         * has one vs two keyframes.
+         */
+        if (!pseudoElement && unresolvedKeyframes.length < 2) {
+            unresolvedKeyframes.unshift(getComputedStyle(element, name))
+        }
+
+        animationOptions.keyframes = unresolvedKeyframes as ValueKeyframe[]
+    }
+
+    /**
+     * Step 3: Create new animations (write)
+     */
+    const animations: AnimationPlaybackControls[] = []
+    for (let i = 0; i < animationDefinitions.length; i++) {
+        const { map, key, options: animationOptions } = animationDefinitions[i]
+        const animation = new NativeAnimation(
+            animationOptions as NativeAnimationOptions
+        )
+
+        map.set(key, animation)
+        animation.finished.finally(() => map.delete(key))
+
+        animations.push(animation)
     }
 
     return animations
