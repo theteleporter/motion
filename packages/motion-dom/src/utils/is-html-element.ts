7b7eb924bc405ce278858f0d1d7c78b24170c11a
Matt Perry <mattgperry@gmail.com>
2025-05-14 16:14:56 +0200

    Extracting isObject

--- packages/motion-dom/src/utils/is-html-element.ts
+++ packages/motion-dom/src/utils/is-html-element.ts
@@ -1,11 +1,9 @@
+import { isObject } from "motion-utils"
+
 /**
  * Checks if an element is an HTML element in a way
  * that works across iframes
  */
 export function isHTMLElement(element: unknown): element is HTMLElement {
-    return (
-        element !== null &&
-        typeof element === "object" &&
-        "offsetHeight" in element
-    )
+    return isObject(element) && "offsetHeight" in element
 }
